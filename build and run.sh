939543b2dc277f2a9fc6417118c11e368a162bf5
sunflower-dreams-123 <jd01838surrey@gmail.com>
2024-05-24 13:04:23 +0100

    commit

--- build and run.sh
+++ build and run.sh
@@ -1,4 +1,4 @@
-#import these codes from github with changes
+# Track changes with Git
 # Builds the model using the data from surrey-nlp/PLOD-CW and the sentence-transformers/all-mpnet-base-v2 model
 python buildmodel.py
 # Runs the model on a webserver in docker using dockerfile and docker-compose
